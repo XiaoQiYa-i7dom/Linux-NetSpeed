b425a5cbcab9cf7a7f8f6f9a63a760b6f00b126a
xiaoqi <1846627226@qq.com>
2019-01-22 15:14:12 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -2,14 +2,6 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-#=================================================
-#	System Required: CentOS 6/7,Debian 8/9,Ubuntu 16+
-#	Description: BBR+BBR魔改版+BBRplus+Lotserver
-#	Version: 1.2.1
-#	Author: 千影,cx9208
-#	Blog: https://www.94ish.me/
-#=================================================
-
 sh_ver="1.2.1"
 github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
 
@@ -343,7 +335,6 @@ Update_Shell(){
 start_menu(){
 clear
 echo && echo -e " TCP加速 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-  -- 就是爱生活 | 94ish.me --
   
  ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
 ————————————内核管理————————————
